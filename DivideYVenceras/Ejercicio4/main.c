ed0bae03b1aab4e740f9439bded021a0b96944cb
GalaxyLaptop <cocolete03@gmail.com>
2023-02-14 11:45:18 +0100

    Terminado ejercicio4

--- DivideYVenceras/Ejercicio4/main.c
+++ DivideYVenceras/Ejercicio4/main.c
@@ -11,12 +11,20 @@ void mayorMenorDYV(int* v, int izq,int der,int* min,int* max){
         *max = *max > maxIzq ? *max : maxIzq;
     }
 }
+int sumaDYV(int* v,int izq ,int der,int menor,int mayor){
+    if(izq == der){
+        return ((v[izq] != mayor) && (v[izq] != menor) )? v[izq] : 0;
+    } else{
+        int mid = (izq + der) / 2;
+        return sumaDYV(v,izq,mid,menor,mayor) + sumaDYV(v,mid + 1,der,menor,mayor);
+    }
+}
 
 int main() {
     int mayor;
     int menor;
     int v[] = {8,7,6,5,4,1};
     mayorMenorDYV(v,0,5,&menor,&mayor);
-    printf("el menor es:%d\n el mayor es:%d",menor,mayor);
+    printf("la suma es:%d", sumaDYV(v,0,5,menor,mayor));
     return 0;
 }
