dd12cff3acc4bd8232d9080fd0c1ee336af286e3
Galaxyraul <cocolete03@gmail.com>
2023-03-13 19:29:29 +0100

    actualizacin para que devuelva suma

--- Voraces/ActividadVoluntaria1/main.c
+++ Voraces/ActividadVoluntaria1/main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-void selector(int x,int y){
+int selector(int x,int y){
     //Comprobamos que ninguno sea cero
     if(!x || !y){
-        printf("Terminando el programa");
-        return;
+        return 0;
     }
     //Sacamos la division y el resto
     int num = (x > y? x : y )/ (x > y? y : x);
     printf("Cuadrados de tamaño %d:%d \n",x > y? y : x,num);
     int rem = (x > y? x : y) % (x > y? y : x);
     //Mandamos el menor y el resto
-    selector(x > y? y : x,rem);
+    return num += selector(x > y? y : x,rem);
 }
 int main() {
-    selector(9,5);
+    printf("El número total de cuadrados que se ha usado es:%d", selector(9,5));
 }
